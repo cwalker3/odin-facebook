764245b61a9a7941909b22898a1d5473c8226553
Cory Walker <cory.walker.dev@gmail.com>
2023-02-15 11:56:38 -0800

    Add create action to liking controller

--- app/controllers/likings_controller.rb
+++ app/controllers/likings_controller.rb
@@ -1,2 +1,17 @@
 class LikingsController < ApplicationController
+  def create
+    @liking = Liking.new(liking_params)
+
+    if @liking.save
+      redirect_back fallback_location: root_path, notice: 'Liked post'
+    else
+      redirect_back fallback_location: root_path, notice: 'Failed to like post'
+    end
+  end
+
+  private
+
+  def liking_params
+    params.permit(:liking).require(:user_id, :liked_id, :kind)
+  end
 end
